271ef64b4809d3f115e2c630b6d6732c5ba1b72a
YJR-y <861195965@qq.com>
2020-11-18 21:08:12 +0800

    微信点单系统

--- src/main/java/com/yjr/ordering_system/service/impl/OrderServiceImpl.java
+++ src/main/java/com/yjr/ordering_system/service/impl/OrderServiceImpl.java
@@ -55,15 +55,6 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderMasterRepository orderMasterRepository;
 
-    public static void main(String[] args) {
-        int[] ints = {1, 2, 3};
-        int sum = 0;
-        for (int i = 0; i < ints.length; i++) {
-            sum = sum + ints[i];
-        }
-        System.out.println(sum);
-    }
-
     /**
      * 创建订单
      *
